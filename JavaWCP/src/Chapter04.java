6173715e7e89fb770be67d5c959d03df3041643a
maisen35 <95683382+maisen35@users.noreply.github.com>
2022-04-29 23:02:42 +0900

    [finish]

---
+++ JavaWCP/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "うんこくん";
+		System.out.println(str);
+		
+		str = "うますぎちゃん";
+		System.out.println(str);
+		
+		int num = 2;
+		System.out.println(num);
+	}
+}
---
+++ JavaWCP/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long a = 112233L;
+		double b = 1122.33;
+		boolean c = true;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		Float d = 1122.33f;
+		Integer e = 3;
+		Character f = 'A';
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		
+		int[] g = { 1, 2, 3, 4, 5 };
+		System.out.println(g[3]);
+	}
+}
