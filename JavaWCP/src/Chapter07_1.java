0dd64205645c0af9149d3204be50a92533a8d5e5
maisen35 <95683382+maisen35@users.noreply.github.com>
2022-05-01 22:11:58 +0900

    [add]

---
+++ JavaWCP/src/Chapter07_1.java
@@ -0,0 +1,15 @@
+
+public class Chapter07_1 {
+	public static void main(String[] args) {
+		int num = 33;
+		if (num <= 5) {
+			System.out.println("こっちくんな");
+		} else if (num <= 10) {
+			System.out.println("なにみてんだよ");
+		} else if (num <= 15) {
+			System.out.println("ばかやろう");
+		} else {
+			System.out.println("とても遠いです");
+		}
+	}
+}
---
+++ JavaWCP/src/Chapter07_2.java
@@ -0,0 +1,22 @@
+
+public class Chapter07_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String color = "red";
+		switch (color) {
+			case "red":
+				System.out.println("asdfa");
+				break;
+			case "yellow":
+				System.out.println("adf");
+				break;
+			case "blue":
+				System.out.println("fsdf");
+				break;
+			default:
+				System.out.println("uuu");
+		}
+	}
+
+}
