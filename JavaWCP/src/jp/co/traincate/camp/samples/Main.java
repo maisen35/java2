c2591cd8da9ad1e6b295d3c00b422e590f2d45c2
maisen35 <95683382+maisen35@users.noreply.github.com>
2022-06-02 10:59:36 +0900

    [add]

---
+++ JavaWCP/src/jp/co/traincate/camp/samples/Main.java
@@ -0,0 +1,12 @@
+package jp.co.traincate.camp.samples;
+
+import jp.co.traincate.camp.samples.window.MainWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // ゲームWindowの生成と表示
+        MainWindow window = new MainWindow();
+        window.show();
+    }
+}
---
+++ JavaWCP/src/jp/co/traincate/camp/samples/enums/Status.java
@@ -0,0 +1,11 @@
+package jp.co.traincate.camp.samples.enums;
+
+/**
+ * ゲームのプレイ状況
+ */
+public enum Status {
+  /** 待ち状態 */
+  Wait,
+  /** 結果発表中 */
+  Done
+}
